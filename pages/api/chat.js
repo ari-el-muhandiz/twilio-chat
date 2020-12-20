3cb3b1c01e4e836faba5d7480dd3f2a3b8573645
Ari Firmanto <ari@waresix.com>
2020-12-20 23:40:58 +0700

    adding cors

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -1,6 +1,19 @@
-import TokenService from '../../library/token';
+import Cors from 'cors'
+import TokenService from '../../library/token'
+import initMiddleware from '../../library/middleware'
 
-export default function handler(req, res) {
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ['GET', 'POST', 'OPTIONS'],
+  })
+)
+
+export default async function handler(req, res) {
+    // Run cors
+    await cors(req, res)
     var identity = req.body.identity;
     var token = TokenService.generate(identity)
   
